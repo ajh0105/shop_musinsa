285492d8b54acc38bf5a0dc8ec951451218e2efc
ajh0105 <ams01055@gmail.com>
2026-03-23 21:46:26 +0900

    내용수정한 벙딸리제ver push

---
+++ ver2/shop_ventalize/shop/src/main/java/com/ventalize/shop/dto/cart/CartItemResponse.java
@@ -0,0 +1,14 @@
+package com.ventalize.shop.dto.cart;
+
+import com.ventalize.shop.dto.item.ItemRead;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CartItemResponse {
+
+    private Integer id;       // cart row id
+    private Integer itemId;
+    private ItemRead item;
+}
